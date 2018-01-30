edb840ca4a8f1cfe40b33d15738db6aedfd1403a
Chaitanya <seechaitanya0405@gmail.com>
2018-01-30 22:48:59 +0530

    Update

---
+++ WebFinalProject/org/main/java/start/WebApplication.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package main.java.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chaitanya.ghevade
+ *
+ */
+@SpringBootApplication
+public class WebApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(WebApplication.class, args);
+	}
+}
