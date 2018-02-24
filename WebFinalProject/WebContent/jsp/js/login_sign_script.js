17ce9da5721e1c77a0e241246c825eb2ed2f93d6
Chaitanya <seechaitanya0405@gmail.com>
2018-02-24 23:30:49 +0530

    Login Page & Spring POM Configuration

--- WebFinalProject/WebContent/jsp/js/login_sign_script.js
+++ WebFinalProject/WebContent/jsp/js/login_sign_script.js
@@ -1,8 +1,18 @@
-var modal = document.getElementById('id01');
+var modal_signup = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal_signup) {
+    	modal_signup.style.display = "none";
     }
+}
+
+
+var modal_login = document.getElementById('id02');
+
+//When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+ if (event.target == modal_login) {
+	 modal_login.style.display = "none";
+ }
 }
