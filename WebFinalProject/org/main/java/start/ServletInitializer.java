d32b85f3316facacf139a821fb2c8702d334d82d
Chaitanya <seechaitanya0405@gmail.com>
2018-02-25 13:18:08 +0530

    POM updated

---
+++ WebFinalProject/org/main/java/start/ServletInitializer.java
@@ -0,0 +1,16 @@
+package main.java.start;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebApplication.class);
+	}
+
+}
